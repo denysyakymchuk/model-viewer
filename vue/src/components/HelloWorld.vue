4f7b2c278a482fa6036c4e8a7ee1b27b9e08e5c1
Denys <hromosoma235@gmail.com>
2023-12-28 16:06:20 +0100

    fix bugs in docker compose file

---
+++ vue/src/components/HelloWorld.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="hello">
+    <carousel :items-to-show="1.5">
+      <slide v-for="slide in 10" :key="slide">
+        {{ slide }}
+      </slide>
+
+      <template #addons>
+        <navigation />
+        <pagination />
+      </template>
+    </carousel>
+  </div>
+</template>
+
+<script>
+import 'vue3-carousel/dist/carousel.css';
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel';
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  components: {
+    Carousel,
+    Slide,
+    Pagination,
+    Navigation,
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
