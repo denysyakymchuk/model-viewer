cf66ed02a1dbec20e95ca65612cf549fecf5ee46
Denys <denys@example.com>
2024-06-22 14:35:22 +0200

    fix bugs, add showing errors, add path field validation

--- vue/src/views/TestPageComponent.vue
+++ vue/src/views/TestPageComponent.vue
@@ -1,6 +1,6 @@
 <template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
   <div>
-    <iframe src="http://localhost/model/26?shadowIntensity=0&brightness=0&contrast=0&opacity=1&blendMode=skip&blockOpacity=1&pixar=1&isEnvImage=1&isSkyBoxImage=1"></iframe>
+    <iframe src="https://modelviewer.pl/model/2?shadowIntensity=0&brightness=0&contrast=0&opacity=1&blendMode=skip&blockOpacity=1&pixar=0&isEnvImage=0&isSkyBoxImage=0"></iframe>
     <h1>Test Page</h1>
   </div>
 </template>
